a46d53e1a3100151a57c05209ea20a75f41a95f1
Arkiver2 <Arkiver@hotmail.com>
2016-12-14 22:24:26 +0100

    Make crawls independent from main process.
    Bump version to 20161214.02.

--- grab.py
+++ grab.py
@@ -1,13 +1,12 @@
-import threading
 import os
 import random
 import string
+import subprocess
+import threading
 import time
+
 import file
-
-
 import settings
-
 
 
 class Grab(threading.Thread):
@@ -23,28 +22,42 @@ class Grab(threading.Thread):
             for filename in os.listdir(settings.dir_new_lists):
                 while not settings.grab_running:
                     time.sleep(1)
-                if filename.startswith('.'):
-                    print(filename + 'Invalid JSON file!')
-                    time.sleep(1)
-                if not filename.startswith('.'):
-                    print filename
-                    filename_urls = os.path.join(settings.dir_old_lists, filename + '_urls')
-                    filejson = file.File(os.path.join(settings.dir_new_lists, filename)).read_json()
-                    if filejson['nick'] != settings.irc_nick:
-                        settings.irc_bot.set_nick(filejson['nick'])
-                    file.File(filename_urls).write_lines(filejson['urls'])
-                    os.rename(os.path.join(settings.dir_new_lists, filename),
-                            os.path.join(settings.dir_old_lists, filename))
-                    self.grabs[filename] = threading.Thread(target=self.grab_single, args=(filename_urls,))
-                    self.grabs[filename].daemon = True
-                    self.grabs[filename].start()
+
                 time.sleep(10)
 
-    @staticmethod
+                if filename.startswith('.'):
+                    print filename + 'Invalid JSON file!' 
+                    continue
 
+                print filename
+                filename_urls = os.path.join(settings.dir_old_lists, filename + '_urls')
+                filejson = file.File(os.path.join(settings.dir_new_lists, filename)).read_json()
+
+                if filejson['nick'] != settings.irc_nick:
+                    settings.irc_bot.set_nick(filejson['nick'])
+
+                file.File(filename_urls).write_lines(filejson['urls'])
+                os.rename(os.path.join(settings.dir_new_lists, filename),
+                        os.path.join(settings.dir_old_lists, filename))
+                self.grabs[filename] = threading.Thread(target=self.grab_single, args=(filename_urls,))
+                self.grabs[filename].daemon = True
+                self.grabs[filename].start()
+
+    @staticmethod
     def grab_single(name):
         video_string = '--youtube-dl ' if '-videos' in name else ''
-        extra_args = ' --1'
-        os.system('~/.local/bin/grab-site --input-file {name} --level=0 --ua="ArchiveTeam; Googlebot/2.1" --no-sitemaps --concurrency=5{extra} --warc-max-size=524288000 --wpull-args="{video}--no-check-certificate --timeout=300" > /dev/null 2>&1'.format(
-            name=name, extra=extra_args, video=video_string))
+        subprocess.Popen([
+                os.path.expanduser('~/.local/bin/grab-site'),
+                '--input-file='+name,
+                '--level=0',
+                '--ua=ArchiveTeam; Googlebot/2.1',
+                '--no-sitemaps',
+                '--concurrency=5',
+                '--1',
+                '--warc-max-size=524288000',
+                '--wpull-args='+video_string+'--no-check-certificate --timeout=300'
+            ],
+            stdout=open(os.devnull, 'w'),
+            stderr=subprocess.STDOUT)
+        time.sleep(60)
         os.remove(name)
